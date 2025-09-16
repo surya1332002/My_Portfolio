04da7fb0f69ee72cd356b095ebfbfcbeea0d31bd
surya1332002 <Suryavardhanreddy13@gmail.com>
2025-09-15 23:55:01 -0500

    undo changes mistake

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './n8n.tsx';   // or './n8n.tsx' if your file is named .tsx
+
 
 createRoot(document.getElementById("root")!).render(<App />);
